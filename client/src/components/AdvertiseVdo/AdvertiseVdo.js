3a55ac24cdd9ca41831eaf3b57e529e6aa8c35de
VasundharaShivankar <vasundharashivankar179@gmail.com>
2024-09-29 12:22:33 +0530

    video

--- client/src/components/AdvertiseVdo/AdvertiseVdo.js
+++ client/src/components/AdvertiseVdo/AdvertiseVdo.js
@@ -6,7 +6,7 @@ export default function AdvertiseVdo() {
       <section >
         <div className="" style={{ width: '100%', height: '500px', overflow: 'hidden' }}>
         <video  style={{height:'800px',width: '100%',maxWidth: '1500px', objectFit: 'cover'}} controls autoPlay muted>
-  <source src="https://pixabay.com/videos/shopping-center-escalators-modern-12876/" type="video/mp4"/>
+  <source src="https://cdn.pixabay.com/video/2017/11/10/12876-242487530_large.mp4" type="video/mp4"/>
 </video>
 
         </div>
