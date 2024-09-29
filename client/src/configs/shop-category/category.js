f196aed1f154a0c3b2127b62e77b06d4096c1f47
kiran-jaswant <kiranjaswant08@gmail.com>
2024-09-29 09:43:31 +0530

    config file created

---
+++ client/src/configs/shop-category/category.js
@@ -0,0 +1,10 @@
+import img1 from './categoryimg/cloths.jpg'
+import img2 from './categoryimg/gadgets.jpg'
+import img3 from './categoryimg/beauty.jpg'
+import img4 from './categoryimg/kitchen.jpg'
+
+const category=[
+    {
+        
+    }
+]
