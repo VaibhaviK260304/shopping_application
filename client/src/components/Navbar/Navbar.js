fca34288c9de6361412a8100156ef531b9a56817
123vkgithub <kumbhalkarvaibhavi@gmail.com>
2024-09-29 11:59:58 +0530

    footer

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -40,7 +40,7 @@ export default function Navbar() {
     <>
       <nav className="navbar fixed-top navbar-expand-lg navbar-dark bg-dark py-3" >
         <div className="container-fluid navsize">
-          <Link className="navbar-brand" to="/">Shopping</Link>
+          <Link className="navbar-brand" to="/Shopping">Shopping</Link>
 
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
